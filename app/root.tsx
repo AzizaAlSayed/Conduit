7a18f45687ebfc51445f559179bb582738ec81a6
laraSaidSayedAhmad <azizasayedahmad@gmail.com>
2021-12-14 15:34:58 +0200

    improve the root

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,16 @@
-import { LiveReload, Outlet } from "remix";
+import {
+  LiveReload,
+  Outlet,
+  Scripts,
+  LinksFunction,
+  Meta,
+  Links,
+  useCatch,
+} from "remix";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: "//demo.productionready.io/main.css" }];
+};
 
 function Document({
   children,
@@ -11,8 +23,9 @@ function Document({
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <title>Conduit </title>
-        <link rel="stylesheet" href="//demo.productionready.io/main.css"></link>
+        <title>Conduit</title>
+        <Meta />
+        <Links />
       </head>
       <body>
         <nav className="navbar navbar-light">
@@ -56,9 +69,24 @@ export default function App() {
 
 export function ErrorBoundary({ error }: { error: Error }) {
   return (
-    <Document title="Uh-oh!">
-      <h1>App Error</h1>
-      <pre>{error.message}</pre>
+    <Document title="Unexpected Error">
+      <div className="error-messages">
+        <h1>App Error</h1>
+        <pre>{error.message}</pre>
+      </div>
+    </Document>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+  return (
+    <Document title={`${caught.status} ${caught.statusText}`}>
+      <div className="error-container">
+        <h1>
+          {caught.status} {caught.statusText}
+        </h1>
+      </div>
     </Document>
   );
 }
